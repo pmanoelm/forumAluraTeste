f410ef30bcdfcf9ea768e330b88813eaab25cd5c
pmanoelm <74067854+pmanoelm@users.noreply.github.com>
2020-11-10 10:44:17 -0300

    Initial commit

---
+++ src/main/java/br/com/paulo/forumAlura/controller/CursoController.java
@@ -0,0 +1,5 @@
+package br.com.paulo.forumAlura.controller;
+
+public class CursoController {
+
+}
---
+++ src/main/java/br/com/paulo/forumAlura/model/StatusTopico.java
@@ -0,0 +1,10 @@
+package br.com.paulo.forumAlura.model;
+
+public enum StatusTopico {
+	
+	NAO_RESPONDIDO,
+	NAO_SOLUCIONADO,
+	SOLUCIONADO,
+	FECHADO;
+
+}
